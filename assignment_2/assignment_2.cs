0b4ad4da10a619cf1e899713f64d2bb04fd8b4d2
bstrdlpn <nguyenphanlong@gmail.com>
2025-04-18 13:35:39 -0700

    first draft of EnumerateFilesRecursively and FormatByteSize methods
    added logic, need to test in linqpad before merge

--- assignment_2/assignment_2.cs
+++ assignment_2/assignment_2.cs
@@ -19,12 +19,61 @@ namespace FileTypeReport {
     // 1. Enumerate all files in a folder recursively
     private static IEnumerable<string> EnumerateFilesRecursively(string path) {
       // TODO: Fill in your code here.
+        if (!Directory.exists (path))
+        {
+            yield break // directory from path does not exist
+        }
+        // yields all files in current directory
+        foreach (string file in Directory.EnumerateFiles (path)) 
+        {
+            try
+            {
+                yield return file; // our generator
+            }
+            catch (UnauthorizedAccessException)
+            {
+                continue; // skips protected folders
+            }
+        }
+        // yields all files in subdirectories
+        foreach (string directory in Directory.EnumerateDirectories (path))
+        {
+            try
+            {
+                foreach (string file in EnumerateFilesRecursively (directory))
+                {
+                    yield return file;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                continue;
+            }
+            catch (PathTooLongException)
+            {
+                continue; // ignores really long paths (happens if we look at root)
+            }
+        }
     }
 
     // Human readable byte size
     private static string FormatByteSize(long byteSize) {
-      // TODO: Fill in your code here.
-    }
+        // TODO: Fill in your code here.
+
+        // array of strings denoting file size unit
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        double formattedSize = byteSize
+        int sizesIndex = 0
+
+        while (formattedSize >= 1024 && sizesIndex < sizes.Length - 1)
+        // Divide the bytesize by 1024, number of times divided moves the 
+        // sizesIndex up to correctly select the most pertinent size
+        // Ex: 3145728 bytes / 1024 twice == "3 MB"
+        {
+        formattedSize /= 1024; 
+        sizesIndex++;
+        }
+        }
 
     // Create an HTML report file
     private static XDocument CreateReport(IEnumerable<string> files) {
