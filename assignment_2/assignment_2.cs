0094235838c2e07251b996b5ff1a0a407d8dd848
gitjutin <thejustindo@gmail.com>
2025-04-19 18:05:11 -0700

    Update assignment_2.cs

--- assignment_2/assignment_2.cs
+++ assignment_2/assignment_2.cs
@@ -31,9 +31,10 @@ namespace FileTypeReport {
       // 2. Process data
       var query =
         from file in files
-        // TODO: Fill in your code here.
+        let ext = Path.GetExtension(file).ToLower() // Stores the lowercase extension for each file
+        group file by ext into fileGroup            // organize all files into groups
         select new {
-          Type =      // TODO: Fill in your code here.
+          Type = fileGroup.Key == "" ? "[no extension]" : fileGroup.Key, // Checks if extension is empty & labels it
           Count =     // TODO: Fill in your code here.
           TotalSize = // TODO: Fill in your code here.
         };
